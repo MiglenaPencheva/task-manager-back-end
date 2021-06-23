8bc9b0f92c3680f02ac92ecfa8c93394644b42dc
Miglena Pencheva <miglena_pencheva@abv.bg>
2021-06-24 00:06:48 +0300

    Format details data

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,29 +1,28 @@
 const router = require('express').Router();
 const { getAll, getAllCompleted, getAllToDo, getMine, create, getOne, remove, complete } = require('../services/taskService');
 const { getUserById } = require('../services/authService');
-// const { showDeleteMsg } = require('../js/app');
 
 router.get('/all', async (req, res) => {
     const tasks = await getAll(req.query.search);
-    let formatedTasks = tasks.map(x => formatTaskDataForRender(x, x.creator, x.completor));
+    let formatedTasks = tasks.map(x => formatDate(x));
     res.render('taskPageAll', { formatedTasks });
 });
 
 router.get('/archive', async (req, res) => {
     const completed = await getAllCompleted(req.query.search);
-    let formatedCompleted = completed.map(x => formatTaskDataForRender(x, x.creator, x.completor));
+    let formatedCompleted = completed.map(x => formatDate(x));
     res.render('taskPageArchive', { formatedCompleted });
 });
 
 router.get('/to-do', async (req, res) => {
     const toDoList = await getAllToDo(req.query.search);
-    let formatedToDoList = toDoList.map(x => formatTaskDataForRender(x, x.creator, x.completor));
+    let formatedToDoList = toDoList.map(x => formatDate(x));
     res.render('taskPageToDo', { formatedToDoList });
 });
 
 router.get('/my-tasks', async (req, res) => {
     const myTasks = await getMine(req.query.search, req.user._id);
-    let formatedMyTasks = myTasks.map(x => formatTaskDataForRender(x, x.creator, x.completor));
+    let formatedMyTasks = myTasks.map(x => formatDate(x));
     res.render('taskPageMine', { formatedMyTasks });
 });
 
@@ -51,13 +50,17 @@ router.post('/create', async (req, res) => {
 router.get('/:id/details', async (req, res) => {
     try {
         let task = await getOne(req.params.id);
-        let creator = await getUserById(task.creator);
-        let completor = await getUserById(task.completor);
+        formatDate(task);
         
-        let formatedTask = formatTaskDataForRender(task, creator, completor)
-        console.log(formatedTask);
-
-        res.render('details', { formatedTask });
+        let creator = await getUserById(task.creator);
+        formatCreator(task, creator);
+        
+        if (task.completor) {
+            let completor = await getUserById(task.completor);
+            formatCompletor(task, completor);
+        }
+        
+        res.render('details', { task });
 
     } catch (error) {
         res.redirect('/404');
@@ -89,25 +92,50 @@ router.all('*', (req, res) => {
 
 module.exports = router;
 
-function formatTaskDataForRender(task, creator, completor) {
- 
-    task.userCreated = creator.username;
-    
+function formatDate(task) {
     let createdAt = task.created_at;
     let timeCreated = createdAt.toString().split(' ');
+
     task.dateCreated = `${timeCreated[2]} ${timeCreated[1]} ${timeCreated[3]}`;
     task.hourCreated = timeCreated[4].slice(0, 5);
-    
-    if (completor) {
-        
-        task.userCompleted = completor.username;
-        
-        let completedAt = task.updated_at;
-        let timeCompleted = completedAt.toString().split(' ');
-        
-        task.dateCompleted = `${timeCompleted[2]} ${timeCompleted[1]} ${timeCompleted[3]}`;
-        task.hourCompleted = timeCompleted[4].slice(0, 5);
-    }
 
     return task;
 }
+
+function formatCreator(task, creator) {
+    task.userCreated = creator.username;
+    return task;
+}
+
+function formatCompletor(task, completor) {
+    task.userCompleted = completor.username;
+
+    let completedAt = task.updated_at;
+    let timeCompleted = completedAt.toString().split(' ');
+
+    task.dateCompleted = `${timeCompleted[2]} ${timeCompleted[1]} ${timeCompleted[3]}`;
+    task.hourCompleted = timeCompleted[4].slice(0, 5);
+
+    return task;
+}
+// function formatTaskDetails(task, creator, completor) {
+
+//     let createdAt = task.created_at;
+//     let timeCreated = createdAt.toString().split(' ');
+
+//     task.dateCreated = `${timeCreated[2]} ${timeCreated[1]} ${timeCreated[3]}`;
+//     task.hourCreated = timeCreated[4].slice(0, 5);
+
+//     if (completor) {
+
+//         task.userCompleted = completor.username;
+
+//         let completedAt = task.updated_at;
+//         let timeCompleted = completedAt.toString().split(' ');
+
+//         task.dateCompleted = `${timeCompleted[2]} ${timeCompleted[1]} ${timeCompleted[3]}`;
+//         task.hourCompleted = timeCompleted[4].slice(0, 5);
+//     }
+
+//     return task;
+// }
