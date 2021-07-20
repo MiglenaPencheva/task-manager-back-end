9f2adf5a69243acc242f290428b3ba343dc5e987
Miglena Pencheva <miglena_pencheva@abv.bg>
2021-07-20 14:20:15 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
-const DATABASE_NAME = 'tasks';
+// const DATABASE_NAME = 'tasks';
 
 const config = {
     development: {
         PORT: process.env.PORT || 3033,
-        DB_URI: `mongodb://localhost/${DATABASE_NAME}`,
+        DB_URI: `mongodb://localhost/tasks`,
         SALT_ROUNDS: 10,
         SECRET: 'STAVAMNOGOSOLENO',
         COOKIE_NAME: 'TOKEN',
